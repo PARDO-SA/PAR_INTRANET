c90f926792560878ec5eac68ed3fdedd9d3d69ae
Victor Malvasio <victormalvasio@gmail.com>
2018-09-21 10:47:21 -0300

    Index Comisiones andando

--- PAR_INTRANET/Scripts/table.js
+++ PAR_INTRANET/Scripts/table.js
@@ -54,5 +54,60 @@ $(document).ready(function () {
         "pageLength": 20,
     });
 
+    $('#tablaComi').dataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        {
+            className: 'btn-nuevo',
+            action: function () {
+                location.href = '/Comisiones/create';
+            }
+        },
+        {
+            text: '',
+            extend: 'excel',
+            className: 'btn-excel',
+            titleAttr: 'Excel'
+        },
+        {
+            text: '',
+            extend: 'pdf',
+            className: 'btn-pdf',
+            titleAttr: 'Pdf'
+
+        },
+        {
+            text: '',
+            extend: 'print',
+            className: 'btn-print',
+            titleAttr: 'Imprimir'
+
+        },
+    ],
+    "language": {
+        //"url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando _START_ al _END_ de _TOTAL_ registros ",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        }
+    },
+    "bLengthChange": false,
+    "pageLength": 20,
+});
+
 
 });
