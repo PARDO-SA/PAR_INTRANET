8063abceb2300806ea232e3db2720bc7b3a4ecb9
Gabriel Garay <garaygl@hotmail.com>
2018-09-25 16:13:38 -0300

    Validacion para Funcion Primaria y SEcundaria de Empleados - Se crea una clase Validaciones.cs para poener alli todas las validaciones por dataanotations...

--- PAR_INTRANET/Models/Clases/Empleado.cs
+++ PAR_INTRANET/Models/Clases/Empleado.cs
@@ -43,10 +43,12 @@ namespace PAR_INTRANET.Models.Clases
         [Required]
         [ForeignKey("FuncionPri")]
         [Display(Name = "Función")]
+        [FuncionEmpleValidator(OtraFuncion = "FuncionS", ErrorMessage = "No puede ser la misma Funcion")]
         public int FuncionP { get; set; }
 
         [ForeignKey("FuncionSec")]
         [Display(Name = "Función Secundaria")]
+        [FuncionEmpleValidator(OtraFuncion = "FuncionP", ErrorMessage = "No puede ser la misma Funcion")]
         public int? FuncionS { get; set; }
 
         [Display(Name = "Estado")]
---
+++ PAR_INTRANET/Models/Clases/Validaciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace PAR_INTRANET.Models.Clases
+{
+    public class FuncionEmpleValidator : ValidationAttribute
+    {
+        public string OtraFuncion { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(OtraFuncion))
+                throw new Exception("No se ha especificado la propiedad 'OtraFuncion'");
+
+            //Obetenemos la propiedad
+            var property = validationContext.ObjectType.GetProperty(OtraFuncion);
+            if (property == null)
+                throw new Exception(string.Format("No se encuentra la propiedad '{0}'", OtraFuncion));
+
+            //Comprobamos el valor
+            int? valor = (int?)property.GetValue(validationContext.ObjectInstance, null);
+
+            return valor == (int?)value ? new ValidationResult(ErrorMessage) : null;
+        }
+    }
+}
