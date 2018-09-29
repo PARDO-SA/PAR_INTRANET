a03d4b64dd4561c3f205c4d2ba3cef8152652303
Gabriel Garay <garaygl@hotmail.com>
2018-09-29 09:03:12 -0300

    Clase Articulo

---
+++ PAR_INTRANET/Models/Clases/Articulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PAR_INTRANET.Models.Clases
+{
+    [Table("Artic")]
+    public class Articulo
+    {
+        [Key]
+        [Display(Name = "Código")]
+        public string CodArt { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string DesArt { get; set; }
+
+        [Display(Name = "Rubro")]
+        [StringLength(3)]
+        public string CodNivArt1Comi { get; set; }
+
+        [Display(Name = "SubRubro")]
+        [StringLength(3)]
+        public string CodNivArt2Comi { get; set; }
+
+        [ScaffoldColumn(false)]
+        [StringLength(3)]
+        public string CodNivArt3Comi { get; set; }
+
+        [Display(Name = "Marca")]
+        [StringLength(10)]
+        public string MarcaComi { get; set; }
+
+        [Display(Name = "Estado")]
+        public bool Inactivo { get; set; }
+    }
+}
